4f7fb90b4679399fead791e771ad59ecc2c056ba
Caleb Lee <caleb.a.lee@icloud.com>
2025-04-06 19:20:09 -0400

    First attempt to implement SMX SDK driver
    sqquashed
    
    Co-Authored-By: sukibaby <163092272+sukibaby@users.noreply.github.com>

---
+++ src/arch/InputHandler/InputHandler_Win32_SMX.h
@@ -0,0 +1,51 @@
+#ifndef INPUT_HANDLER_WIN32_SMX_H
+#define INPUT_HANDLER_WIN32_SMX_H
+
+#include "InputHandler.h"
+
+constexpr int SMX_FAILURE = -1;
+constexpr int SMX_AMBIGUOUS = 0;
+constexpr int SMX_SUCCESS = 1;
+constexpr int SMX_PAD_COUNT = 2;
+constexpr int SMX_PANEL_COUNT = 9;
+
+bool InputHandler_Win32_SMX_Is_SMX_DLL_Available();
+void InputHandler_Win32_SMX_Register_Pad();
+
+enum SMXUpdateCallbackReason {
+	SMXUpdateCallback_Updated,
+	SMXUpdateCallback_FactoryResetCommandComplete
+};
+typedef void SMXUpdateCallback(int pad, SMXUpdateCallbackReason reason, void* pUser);
+struct SMXInfo {
+    bool m_bConnected = false;
+	char m_Serial[33];
+	uint16_t m_iFirmwareVersion;
+};
+typedef void SMXLogCallback(const char *log);
+
+class InputHandler_Win32_SMX: public InputHandler
+{
+public:
+	InputHandler_Win32_SMX();
+	~InputHandler_Win32_SMX();
+	void GetDevicesAndDescriptions( std::vector<InputDeviceInfo>& vDevicesOut );
+	RString GetDeviceSpecificInputString( const DeviceInput &di );
+	void ProcessInputEvent( int pad );
+
+	void SMX_Start();
+
+private:
+	uint16_t m_padInputStates[SMX_PAD_COUNT];
+	int IsPadConnected();
+	int GetStageStatus();
+	int InitializeSMX();
+	void LaunchSDK();
+
+	void SMX_GetInfo( int pad, struct SMXInfo *info );
+    uint16_t SMX_GetInputState( int pad );
+	void SMX_SetLogCallback();
+	void SMX_Stop();
+};
+
+#endif
