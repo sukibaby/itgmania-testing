f52493b9928dfea13789ceb5e10add5db232c2d8
sukibaby <163092272+sukibaby@users.noreply.github.com>
2025-04-18 05:46:22 -0700

    Remove ScoreKeeperRave from CMake

--- src/CMakeData-data.cmake
+++ src/CMakeData-data.cmake
@@ -121,13 +121,11 @@ source_group("Data Structures\\\\Notes Writers"
 list(APPEND SM_DATA_SCORE_SRC
             "ScoreKeeper.cpp"
             "ScoreKeeperNormal.cpp"
-            "ScoreKeeperRave.cpp"
             "ScoreKeeperShared.cpp")
 
 list(APPEND SM_DATA_SCORE_HPP
             "ScoreKeeper.h"
             "ScoreKeeperNormal.h"
-            "ScoreKeeperRave.h"
             "ScoreKeeperShared.h")
 
 source_group("Data Structures\\\\Score Keepers"
