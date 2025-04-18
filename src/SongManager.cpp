ed301910e8a13748cfd7a70a77d33ea89a47e849
sukibaby <163092272+sukibaby@users.noreply.github.com>
2025-04-18 04:37:58 -0700

    Revert "Update src/SongManager.cpp"
    This reverts commit fc98959db49079136ad8c235b134f504ccdcb285.

--- src/SongManager.cpp
+++ src/SongManager.cpp
@@ -281,10 +281,7 @@ void SongManager::AddGroup( RString sDir, RString sGroupDirName, Group* group )
 
 	m_sSongGroupNames.push_back( sGroupDirName );
 	// add to the group list
-	if( group )
-	{
-		m_pGroups.push_back( group );
-	}
+	m_pGroups.push_back( group );
 	
 	if (m_mapGroupsByName.find(sGroupDirName) == m_mapGroupsByName.end())
 	{
