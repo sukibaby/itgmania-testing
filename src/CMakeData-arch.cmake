c0e0240495c235d268690118c9c0ba6bc971e983
sukibaby <163092272+sukibaby@users.noreply.github.com>
2025-05-21 12:45:32 -0700

    Remove WDMKS audio driver
    Proposing removal of this driver to facilitate the movement from RString to std::string.
    
    WDMKS has been unmaintained since Windows Vista was released. Only DirectSound or WASAPI are recommended since Vista.

--- src/CMakeData-arch.cmake
+++ src/CMakeData-arch.cmake
@@ -36,13 +36,11 @@ if(WIN32)
   list(APPEND SMDATA_ARCH_SOUND_SRC
               "arch/Sound/DSoundHelpers.cpp"
               "arch/Sound/RageSoundDriver_DSound_Software.cpp"
-              "arch/Sound/RageSoundDriver_WaveOut.cpp"
-              "arch/Sound/RageSoundDriver_WDMKS.cpp")
+              "arch/Sound/RageSoundDriver_WaveOut.cpp")
   list(APPEND SMDATA_ARCH_SOUND_HPP
               "arch/Sound/DSoundHelpers.h"
               "arch/Sound/RageSoundDriver_DSound_Software.h"
-              "arch/Sound/RageSoundDriver_WaveOut.h"
-              "arch/Sound/RageSoundDriver_WDMKS.h")
+              "arch/Sound/RageSoundDriver_WaveOut.h")
 elseif(APPLE)
   list(APPEND SMDATA_ARCH_SOUND_SRC "arch/Sound/RageSoundDriver_AU.mm")
   list(APPEND SMDATA_ARCH_SOUND_HPP "arch/Sound/RageSoundDriver_AU.h")
