96f99e536e212d0645bd4a8def2874a7f068ab83
sukibaby <163092272+sukibaby@users.noreply.github.com>
2025-09-25 02:47:25 -0700

    Update CPackSetup for WiX Toolset
    Migrating from NSIS to WiX Toolset to easily generate a .msi installer for Windows platforms. WiX can be installed with Chocolatey in admin PowerShell with choco install wixtoolset

--- CMake/CPackSetup.cmake
+++ CMake/CPackSetup.cmake
@@ -14,46 +14,27 @@ set(CPACK_RESOURCE_FILE_README "${SM_ROOT_DIR}/README.md")
 set(CPACK_RESOURCE_FILE_LICENSE "${SM_CMAKE_DIR}/license_install.txt")
 
 if(WIN32)
-  set(CPACK_GENERATOR NSIS)
+
+  # WiX Toolset is used to build a MSI installer.
+  # This is typically provided by the Visual Studio Installer,
+  # but is also available via chocolatey, etc.
+  # https://cmake.org/cmake/help/latest/cpack_gen/wix.html
+  set(CPACK_GENERATOR WIX)
   set(CPACK_SYSTEM_NAME "Windows")
 
-  # By setting these install keys manually, The default directory of "StepMania
-  # major.minor.patch" is lost. This is currently done to maintain backwards
-  # compatibility. However, removing these two will allow for multiple versions
-  # of StepMania to be installed relatively cleanly.
-  set(CPACK_PACKAGE_INSTALL_DIRECTORY "ITGmania")
-  set(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "ITGmania")
-  set(CPACK_NSIS_EXECUTABLES_DIRECTORY "Program")
-  set(CPACK_NSIS_INSTALL_ROOT "C:\\\\Games")
+  # GUID should remain unchanged. (helps detect old version installs, etc)
+  set(CPACK_WIX_UPGRADE_GUID "a1dcf4ac-e756-4625-8b53-2584e8b7a69a")
 
-  set(CPACK_NSIS_HELP_LINK "https://github.com/itgmania/itgmania/issues")
-  set(CPACK_NSIS_PACKAGE_NAME "${SM_EXE_NAME} ${CPACK_PACKAGE_VERSION}")
-  set(CPACK_NSIS_URL_INFO_ABOUT "https://www.itgmania.com/")
-  set(CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL ON)
-  set(CPACK_NSIS_MUI_ICON "${SM_INSTALLER_DIR}/install.ico")
-  set(CPACK_NSIS_MUI_UNIICON "${SM_INSTALLER_DIR}/uninstall.ico")
-  set(CPACK_NSIS_MUI_HEADERIMAGE "${SM_INSTALLER_DIR}/header.bmp")
-  set(CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP "${SM_INSTALLER_DIR}/welcome.bmp")
-  set(CPACK_NSIS_COMPRESSOR "/SOLID lzma")
-  set(CPACK_NSIS_MUI_FINISHPAGE_RUN "${SM_EXE_NAME}.exe")
-  set(CPACK_NSIS_BRANDING_TEXT " ")
-
-  # The header and welcome bitmaps require backslashes.
-  string(REGEX
-         REPLACE "/"
-                 "\\\\\\\\"
-                 CPACK_NSIS_MUI_HEADERIMAGE
-                 "${CPACK_NSIS_MUI_HEADERIMAGE}")
-  string(REGEX
-         REPLACE "/"
-                 "\\\\\\\\"
-                 CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP
-                 "${CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP}")
-
-  # Custom items for nsis go here.
-  set(CPACK_SM_NSIS_PRODUCT_ID "ITGmania")
-  set(CPACK_SM_NSIS_PRODUCT_VERSION "${SM_VERSION_TRADITIONAL}.0")
-  set(CPACK_SM_NSIS_GIT_VERSION "${SM_VERSION_GIT}")
+  set(CPACK_WIX_SKIP_PROGRAM_FOLDER ON)
+  set(CPACK_PACKAGE_INSTALL_DIRECTORY "C:\\Games\\ITGmania")
+  set(CPACK_WIX_PRODUCT_ICON "${SM_INSTALLER_DIR}/install.ico")
+  set(CPACK_WIX_UI_BANNER "${SM_INSTALLER_DIR}/header.bmp")
+  set(CPACK_WIX_PROGRAM_MENU_FOLDER "ITGmania")
+  set(CPACK_WIX_HELP_LINK "https://github.com/itgmania/itgmania/issues")
+  set(CPACK_WIX_PRODUCT_NAME "${SM_EXE_NAME} ${CPACK_PACKAGE_VERSION}")
+  set(CPACK_WIX_URL_INFO_ABOUT "https://www.itgmania.com/")
+  set(CPACK_WIX_PROPERTY_ARPURLINFOABOUT "https://www.itgmania.com/")
+  
 elseif(MACOSX)
   set(CPACK_GENERATOR DragNDrop)
   set(CPACK_DMG_VOLUME_NAME "${CPACK_PACKAGE_NAME} ${CPACK_PACKAGE_VERSION}")
