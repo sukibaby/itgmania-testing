1e8b1169d6517be6d0051b4a01004bbf96860c3f
sukibaby <163092272+sukibaby@users.noreply.github.com>
2025-09-29 14:23:42 -0700

    Update global.h

--- src/global.h
+++ src/global.h
@@ -7,12 +7,20 @@
 #pragma once
 #endif
 
-/** @brief This macro is for INT8_MIN, etc. */
+//
+// +---------------------------------+
+// | Standard macros for integer     |
+// | limits and constants            |
+// +---------------------------------+
+//
 #define __STDC_LIMIT_MACROS
-/** @brief This macro is for INT64_C, etc. */
 #define __STDC_CONSTANT_MACROS
 
-/* Platform-specific fixes. */
+//
+// +-----------------------------+
+// | Platform-specific setup     |
+// +-----------------------------+
+//
 #if defined(_WIN32)
 #include "archutils/Win32/arch_setup.h"
 #elif defined(PBBUILD)
@@ -21,10 +29,20 @@
 #include "archutils/Unix/arch_setup.h"
 #endif
 
-/* Make sure everyone has min and max: */
+//
+// +-----------------------------+
+// | Standard library includes   |
+// +-----------------------------+
+//
 #include <algorithm>
+#include <utility>
 
-/* Branch optimizations: */
+//
+// +-------------------------------------+
+// | Branch optimization hints           |
+// | (likely, unlikely)                  |
+// +-------------------------------------+
+//
 #if defined(__GNUC__)
 #define likely(x) (__builtin_expect(!!(x), 1))
 #define unlikely(x) (__builtin_expect(!!(x), 0))
@@ -33,95 +51,111 @@
 #define unlikely(x) (x)
 #endif
 
+//
+// +---------------------------------+
+// | Ensure ASSERT is not predefined |
+// +---------------------------------+
+//
 #ifdef ASSERT
 #undef ASSERT
 #endif
 
+//
+// +-----------------------------+
+// | RString                      |
+// | TODO: Move to std::string.   |
+// +-----------------------------+
+//
 #include "StdString.h"
-/** @brief Use RStrings throughout the program. */
 typedef StdString::CStdString RString;
 
-/** @brief A set of RStrings. Macro so we don't actually have to pull in std::set here. */
-#define RStringSet std::set<RString>
-
+//
+// +-----------------------------+
+// | Threading / Checkpoints     |
+// +-----------------------------+
+//
 #include "RageThreads.h"
 
+//
+// +-----------------------------+
+// | Crash handling              |
+// +-----------------------------+
+//
+[[noreturn]] void sm_crash(const RString& reason);
+[[noreturn]] void sm_crash(const char* reason = "Internal error");
 
-
-
-/**
- * @brief A crash has occurred, and we're not getting out of it easily.
- *
- * For most users, this will result in a crashinfo.txt file being generated.
- * For anyone that is using a debug build, a debug break will be thrown to
- * allow viewing the current process.
- * @param reason the crash reason as determined by prior function calls.
- * @return nothing: there is no escape without quitting the program.
- */
-[[noreturn]]
-void sm_crash(const RString& reason);
-[[noreturn]]
-void sm_crash( const char *reason = "Internal error" );
-
-/**
- * @brief Assertion that sets an optional message and brings up the crash
- * handler, so we get a backtrace.
- *
- * This should probably be used instead of throwing an exception in most
- * cases we expect never to happen (but not in cases that we do expect,
- * such as DSound init failure.) */
+//
+// +-----------------------------+
+// | Assertion macros            |
+// +-----------------------------+
+//
 #define FAIL_M(MESSAGE) do { CHECKPOINT_M(MESSAGE); sm_crash(MESSAGE); } while(0)
 #define ASSERT_M(COND, MESSAGE) do { if(unlikely(!(COND))) { FAIL_M(MESSAGE); } } while(0)
-
 
 #if !defined(CO_EXIST_WITH_MFC)
 #define ASSERT(COND) ASSERT_M((COND), "Assertion '" #COND "' failed")
 #endif
 
-/** @brief Use this to catch switching on invalid values */
-#define DEFAULT_FAIL(i) 	default: FAIL_M( ssprintf("%s = %i", #i, (i)) )
+#define DEFAULT_FAIL(i) default: FAIL_M(ssprintf("%s = %i", #i, (i)))
 
-void ShowWarningOrTrace( const char *file, int line, const RString& message, bool bWarning );
-void ShowWarningOrTrace( const char *file, int line, const char *message, bool bWarning ); // don't pull in LOG here
+//
+// +-----------------------------+
+// | Warning and tracing         |
+// +-----------------------------+
+//
+void ShowWarningOrTrace(const char* file, int line, const RString& message, bool bWarning);
+void ShowWarningOrTrace(const char* file, int line, const char* message, bool bWarning);
 #define WARN(MESSAGE) (ShowWarningOrTrace(__FILE__, __LINE__, MESSAGE, true))
+
 #if !defined(CO_EXIST_WITH_MFC)
 #define TRACE(MESSAGE) (ShowWarningOrTrace(__FILE__, __LINE__, MESSAGE, false))
 #endif
 
+//
+// +-----------------------------+
+// | Debug assertions            |
+// +-----------------------------+
+//
 #ifdef DEBUG
-// No reason to kill the program. A lot of these don't produce a crash in NDEBUG so why stop?
-// TODO: These should have something you can hook a breakpoint on.
-#define DEBUG_ASSERT_M(COND,MESSAGE) if(unlikely(!(COND))) WARN(MESSAGE)
-#define DEBUG_ASSERT(COND) DEBUG_ASSERT_M(COND,"Debug assert failed")
+#define DEBUG_ASSERT_M(COND, MESSAGE) if(unlikely(!(COND))) WARN(MESSAGE)
+#define DEBUG_ASSERT(COND) DEBUG_ASSERT_M(COND, "Debug assert failed")
 #else
-/** @brief A dummy define to keep things going smoothly. */
 #define DEBUG_ASSERT(x)
-/** @brief A dummy define to keep things going smoothly. */
-#define DEBUG_ASSERT_M(x,y)
+#define DEBUG_ASSERT_M(x, y)
 #endif
 
-/* Use SM_UNIQUE_NAME to get the line number concatenated to x. This is useful for
- * generating unique identifiers in other macros.  */
-#define SM_UNIQUE_NAME3(x,line) x##line
-#define SM_UNIQUE_NAME2(x,line) SM_UNIQUE_NAME3(x, line)
+//
+// +-------------------------------------+
+// | Unique name generation for macros   |
+// +-------------------------------------+
+//
+#define SM_UNIQUE_NAME3(x, line) x##line
+#define SM_UNIQUE_NAME2(x, line) SM_UNIQUE_NAME3(x, line)
 #define SM_UNIQUE_NAME(x) SM_UNIQUE_NAME2(x, __LINE__)
 
+//
+// +-----------------------------+
+// | Exceptions                  |
+// +-----------------------------+
+//
 #include "RageException.h"
 
-// Call a function every `n` frames.
-// Each call site will get its own counter.
+//
+// +-------------------------------------------------+
+// | Utility template: Call a function every N frames |
+// | TODO: Move to a more appropriate location.       |
+// +-------------------------------------------------+
+//
 #include <utility>
 template <typename Func, typename... Args>
 void CallEveryNFrames(int n, Func&& f, Args&&... args) {
-	static int counter = 0;
-	++counter;
-	if (counter == n) {
-		counter = 0;
-		std::forward<Func>(f)(std::forward<Args>(args)...);
-	}
+    static int counter = 0;
+    ++counter;
+    if (counter == n) {
+        counter = 0;
+        std::forward<Func>(f)(std::forward<Args>(args)...);
+    }
 }
-
-/* Don't include our own headers here, since they tend to change often. */
 
 #endif
 
