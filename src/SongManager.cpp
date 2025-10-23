878aec62af758d5734fe13618ea82fed716da112
sukibaby <163092272+sukibaby@users.noreply.github.com>
2025-10-22 23:55:40 -0700

    Replace std::map with std::unordered_map in SongManager
    Changed the type of mapGroupSongDirs from std::map to std::unordered_map in SongManager.cpp for potentially improved lookup performance.

--- src/SongManager.cpp
+++ src/SongManager.cpp
@@ -46,6 +46,7 @@
 #include <vector>
 #include <mutex>
 #include <future>
+#include <unordered_map>
 
 
 namespace std {
@@ -395,7 +396,7 @@ void SongManager::LoadSongDir( RString sDir, LoadingWindow *ld, bool onlyAdditio
 	StripCvsAndSvn( arrayGroupDirs );
 	StripMacResourceForks( arrayGroupDirs );
 
-	std::map<RString, std::vector<RString>> mapGroupSongDirs;
+	std::unordered_map<RString, std::vector<RString>> mapGroupSongDirs;
 	int groupIndex, songCount, songIndex;
 
 	groupIndex = 0;
