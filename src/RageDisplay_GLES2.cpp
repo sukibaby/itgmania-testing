cd9e1b324fe57882010e19896521872687488f1c
sukibaby <163092272+sukibaby@users.noreply.github.com>
2025-11-03 04:47:56 -0800

    Remove frame limiting calls from EndFrame methods
    Eliminated calls to FrameLimitBeforeVsync and FrameLimitAfterVsync in EndFrame methods for D3D, GLES2, and OGL display backends. This simplifies frame presentation and may address timing or compatibility issues related to frame limiting.

--- src/RageDisplay_GLES2.cpp
+++ src/RageDisplay_GLES2.cpp
@@ -435,9 +435,8 @@ void RageDisplay_GLES2::EndFrame()
 	glFlush();
 
 	// XXX: This is broken on NVidia, as their xrandr sucks.
-	FrameLimitBeforeVsync( g_pWind->GetActualVideoModeParams().rate );
+	// (comment may be outdated?)
 	g_pWind->SwapBuffers();
-	FrameLimitAfterVsync();
 
 	g_pWind->Update();
 
