9122cd811be3c1e06e1d8b8a28150eec55492e02
sukibaby <163092272+sukibaby@users.noreply.github.com>
2026-03-25 04:28:25 -0700

    Update StepParityDatastructs.cpp

--- src/StepParityDatastructs.cpp
+++ src/StepParityDatastructs.cpp
@@ -162,16 +162,18 @@ void StageLayout::preCalculateStuff() {
 // Counts the number of ones in the binary representation
 // of the given `x`.
 // TODO: use std::popcount if we upgrade to C++20.
-inline int popcount(unsigned int x) {
+inline int popcount(uint64_t x) {
 #if defined(__clang__) || defined(__GNUC__)
-  return __builtin_popcount(x);
+  return __builtin_popcountll(x);
 #else
-  // https://en.wikipedia.org/wiki/Hamming_weight
+  // popcount64b from https://en.wikipedia.org/wiki/Hamming_weight
   x -= (x >> 1) & m1;              // put count of each 2 bits into those 2 bits
   x = (x & m2) + ((x >> 2) & m2);  // put count of each 4 bits into those 4 bits
   x = (x + (x >> 4)) & m4;         // put count of each 8 bits into those 8 bits
-  return (x * h01) >>
-         56;  // returns left 8 bits of x + (x<<8) + (x<<16) + (x<<24) + ...
+  x += x >> 8;   // put count of each 16 bits into their lowest 8 bits
+  x += x >> 16;  // put count of each 32 bits into their lowest 8 bits
+  x += x >> 32;  // put count of each 64 bits into their lowest 8 bits
+  return x & 0x7f;
 #endif
 }
 
